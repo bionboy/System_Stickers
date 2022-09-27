c04d1a483f5dfc71bed56104f40c589cad55506c
bionboy <luke.floden@gmail.com>
2022-09-27 10:43:59 -0400

    chore: Cleanup of code

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
-const {app, BrowserWindow, remote} = require('electron')
-const path = require('path')
+const { app, BrowserWindow, remote } = require("electron");
 
-let mainWindow
+let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 300,
     height: 300,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+  });
 
-  mainWindow.loadFile('index.html')
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+  mainWindow.loadFile("index.html");
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
-  if (mainWindow === null) createWindow()
-})
+app.on("activate", function () {
+  if (mainWindow === null) createWindow();
+});
